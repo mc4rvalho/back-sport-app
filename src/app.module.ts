fb6e6f017b18b6a23826a2fcaed45b45e79fd5d5
mc4rvalho <mcarvalho093@gmail.com>
2026-03-10 21:33:49 -0300

    chore: add Prisma Module/Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.development.env',
     }),
+    PrismaModule,
   ],
   controllers: [],
   providers: [],
